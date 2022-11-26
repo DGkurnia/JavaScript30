cb6a484349695f47147458c6555b53ea010d5298
DGkurnia <weerakurnia@gmail.com>
2022-11-26 15:56:41 +0700

    revision
    second revision

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -1,5 +1,31 @@
-const video = document.querySelector('.player');
-const canvas = document.querySelector('.photo');
-const ctx = canvas.getContext('2d');
+const player = document.querySelector('.player');
+const images = document.querySelector('.photo');
+const twoDshot = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
-const snap = document.querySelector('.snap');
+const cheese = document.querySelector('.snap');
+
+function videoProject(){
+    navigator.mediaDevices.getUserMedia({video: true, audio: false})
+    .then(media =>{
+        console.log(media)
+        player.srcObject = media
+        player.play()
+    })
+    .catch(err =>{
+        console.error(`Video unavaliable`,err)
+    })
+}
+
+function paintingCanvas(){
+    const wvalue = player.videoWidth
+    const hvalue = player.videoHeight
+    images.width = wvalue
+    images.height = hvalue
+
+    return intervalTime(()=>{
+        ctx.drawImage(player, 0, 0, wvalue,hvalue)
+        let pxvalue = ctx.getImageData(player, 0, 0, wvalue,hvalue)
+        pxvalue = rgbSplit(pxvalue)
+        ctx.putImageData(pxvalue, 0, 0)
+    }, 20)
+}
