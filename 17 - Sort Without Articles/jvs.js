f2ab279d7d22ccf2be01b996c5f1e578033cabcb
DGkurnia <weerakurnia@gmail.com>
2022-11-30 20:02:52 +0700

    revision
    the third revision

---
+++ 17 - Sort Without Articles/jvs.js
@@ -0,0 +1,8 @@
+const bands = ['My Chemical Romance', 
+'Linkin Park', 
+'The Changcuters', 'Kana-Boon', 'Dewa 19', 'U2', 
+'The Midway State', 'Cold Play', 'Marron 5', 'Union Underground', 'Da Ice',
+ 'The Beatles', 'An Old Dog', 'The Chainsmokers'];
+
+ const sorted = bands.sort((a, b) => a - b)
+ console.log(sorted)
--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -23,9 +23,64 @@ function paintingCanvas(){
     images.height = hvalue
 
     return intervalTime(()=>{
-        ctx.drawImage(player, 0, 0, wvalue,hvalue)
-        let pxvalue = ctx.getImageData(player, 0, 0, wvalue,hvalue)
+        twoDshot.drawImage(player, 0, 0, wvalue,hvalue)
+        let pxvalue = twoDshot.getImageData(player, 0, 0, wvalue,hvalue)
         pxvalue = rgbSplit(pxvalue)
-        ctx.putImageData(pxvalue, 0, 0)
+        twoDshot.putImageData(pxvalue, 0, 0)
     }, 20)
 }
+
+function photoImg(){
+    cheese.currentTime = 0
+    cheese.play()
+
+    const imgData = images.toDataURL('images/jpeg','images/png')
+    const imgLink = document.createElement('a')
+    imgLink.href = imgData
+    imgLink.setAttribute('downlolad','facial')
+    imgLink.innerHTML = `<img src = "${imgData}" alt = "Facial Images">`
+    strip.insertBefore(imgLink, strip.firstChild)
+
+}
+
+function rgbSplit(px) {
+    const pxdata = pixels.data
+    const pxlength = pxdata.length
+    for (let a = 0; a < pxlength; a+=4) {
+        px.data[a - 150] = px.data[a + 0]
+        px.data[a + 500] = px.data[a + 1]
+        px.data[a - 550] = px.data[a + 2]
+    }
+    return px
+}
+
+function screenDisplay(px) {
+    const levels = {}
+    const pxdata2 = pixels.data
+    const pxlength2 = pxdata2.length
+    document.querySelectorAll('.rgb input').forEach((i)=>{
+        levels[i.name] = i.value
+    })
+    for (let a = 0; a < pxlength2; a++) {
+        red = pxdata2[a + 0]
+        green = pxdata2[a + 1]
+        blue = pxdata2[a + 2]
+        alpha = pxdata2[a + 3]
+
+        if (red >= levels.rmin
+            && green >= levels.gmin
+            && blue >= levels.bmin
+            && red <= levels.rmax
+            && green <= levels.gmax
+            && blue <= levels.bmax) {
+            // take it out!
+            pxdata2[a + 3] = 0;
+          }
+    }
+
+    return px
+}
+
+videoProject()
+
+player.addEventListener('canplay', paintingCanvas)
